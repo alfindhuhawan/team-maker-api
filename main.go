7aab33e40cf540c7cbf39f35bc1e9bb2e57b4814
Alfin <alfindhuhawanbagja@gmail.com>
2025-05-31 22:00:06 +0700

    add config json

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 	"team-maker-api/delivery/routes"
 	"team-maker-api/repository"
 	"team-maker-api/usecase"
+	"team-maker-api/utils"
 )
 
 func main() {
 	config.LoadConfig("config.json")
-	// utils.InitDB()
+	utils.InitDB()
 
 	userRepo := repository.NewUserRepository()
 	authUsecase := usecase.NewAuthUsecase(userRepo)
